0c181f4571b32b181501a7e8d65eb611985b78fc
WebsiteDisplayName <rowemg@bc.edu>
2022-11-08 01:40:02 -0500

    gg ez

--- pptxToPDF.py
+++ pptxToPDF.py
@@ -1,8 +1,20 @@
 # https://stackoverflow.com/questions/31487478/how-to-convert-a-pptx-to-pdf-using-python
-import win32com.client
+
+# https://stackoverflow.com/questions/20891787/an-efficient-way-to-convert-document-to-pdf-format
+
+
+# https://youtu.be/mlOO9Rp5s8A
+# Python 3 Script To Convert Single Or Multiple Powerpoint (.PPTX) Files to PDF Using ppt2pdf Module
+# pip install ppt2pdf
+# ppt2pdf dir ppt
+# for the ppt directory or: ppt2pdf dir .
+#  ^ for current working directory
+# change drive https://stackoverflow.com/questions/11065421/command-prompt-wont-change-directory-to-another-drive
+
+
 from tkinter.filedialog import askdirectory
 import os
-from pathlib import Path
+import win32com.client
 
 
 def PPTtoPDF(inputFileName, outputFileName, formatType=32):
@@ -17,18 +29,21 @@ def PPTtoPDF(inputFileName, outputFileName, formatType=32):
     powerpoint.Quit()
 
 
-pptxFolder = askdirectory(title='Select Folder To Save Songs In')
+pptxFolder = askdirectory(title='Select Folder with .pptx')
 
 for subdir, dirs, files in os.walk(pptxFolder):
     for i in files:
         if i.endswith(".pdf"):
             continue
         else:
-            fileName = Path(i).stem
-            PPTtoPDF(fileName, fileName)
+            # pptxFilePath = os.path.join(pptxFolder, i)
+            pptxFilePath = pptxFolder + "/" + i
+            pptxFilePath = pptxFilePath.replace("/", "\\")
+            print(pptxFilePath)
+            PPTtoPDF(pptxFilePath, pptxFilePath)
 
     for i in files:
         if i.endswith(".pdf"):
             continue
-        else:
+        elif i.endswith(".pptx"):
             os.remove(os.path.join(pptxFolder, i))
